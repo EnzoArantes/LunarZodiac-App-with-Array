392be3f94b71106ce5a125c228c002a3328c710f
Zo-hack <enzoarantes2000@gmail.com>
2025-01-27 17:47:07 -0500

    Initial Commit

---
+++ LunarZodiac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunarZodiac
+//
+//  Created by Enzo Arantes on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Enzo Arantes on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
